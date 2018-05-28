5fbcb35b9a85116f47911efc6473a32cf2efa42a
Mchoeti <mchoeti@gmail.com>
2018-05-28 09:38:56 +0200

    simples
    Signed-off-by: Mchoeti <mchoeti@gmail.com>

--- First.js
+++ First.js
@@ -1,2 +1,14 @@
-console.log("Hello World");
-console.log("Another world");
+// welcome to a single and simple line 
+
+// declare some variables
+var line;
+var number = 10;
+
+//do a simple for loop
+for (i = 0 ; i <= number ; i++) {
+    console.log("This is just a simple loop where number = " + i);
+}
+
+
+// thank the audience 
+console.log("Hello World this was just a simple one");
