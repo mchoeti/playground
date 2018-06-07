b17d015a3b51bb8c3fac1e521edc95e2c1031223
Mchoeti <lastcrusade@gmx.at>
2018-06-07 13:13:48 +0200

    FCC Tests

---
+++ FCC/simple.js
@@ -0,0 +1,13 @@
+
+// simple convertToInteger Function
+function convertToInteger(str) {
+    return parseInt(str)
+  }
+  
+  //console.log für testt, hier wird eine 7
+  console.log(convertToInteger("000007"));
+  //Aufruf der Funktion
+  convertToInteger("56");
+
+  //Done
+  
