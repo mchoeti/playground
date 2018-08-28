03d95f0df040254a54a50a70446c03d475ec5770
Hoeserle <lastcrusade@gmx.at>
2018-08-28 15:17:09 +0200

    Debugging with Chrome part 1

---
+++ FCC/JS/debug.js
@@ -0,0 +1,12 @@
+// Schnelle und einfach session die anzeigen soll wie einfach es ist mit Developer Tools zu debuggen.
+
+// Anzeigen des Types
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Add your code below this line
+console.log("Ich bin ein(e): " +  typeof seven);
+console.log("Ich bin ein(e): " +  typeof three);
+
+// console.log clearen klappt aber nur im Browser 
+console.clear();
