0b0921c1747369e393972668b4315a61482003b1
Mchoeti <lastcrusade@gmx.at>
2018-11-07 16:47:13 +0100

    hm..

--- FCC/JS/JS_Challanges/ReturnLargesNumbers.js
+++ FCC/JS/JS_Challanges/ReturnLargesNumbers.js
@@ -15,4 +15,4 @@ function largestOfFour(arr) {
   return largestNumber;
 }
   
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
