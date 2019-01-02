69b46cd4becdbfd849a178301807fd5d669113dc
Hoeserle <lastcrusade@gmx.at>
2019-01-02 16:26:52 +0100

    ToDO , Negative Werte abfangen

--- FCC/JS/JS_Challanges/ReturnLargesNumbers.js
+++ FCC/JS/JS_Challanges/ReturnLargesNumbers.js
@@ -16,3 +16,7 @@ function largestOfFour(arr) {
 }
   
 console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+// To Do, weil derzeit werden die negativen Werte nicht berücksichtigt. 
+// Eingabe von 
+console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]))
+// ergibt: 25, 48, 21, 0
