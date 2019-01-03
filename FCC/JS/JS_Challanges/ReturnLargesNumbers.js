8fe5f2c81c616257b215bc5fdc1fa0712e7b65c5
Hoeserle <lastcrusade@gmx.at>
2019-01-03 09:54:46 +0100

    Done

--- FCC/JS/JS_Challanges/ReturnLargesNumbers.js
+++ FCC/JS/JS_Challanges/ReturnLargesNumbers.js
@@ -1,22 +1,24 @@
-function largestOfFour(arr) {
-    // Erstellen eines ersten Arrays
-    var largestNumber = [0,0,0,0];
-    // Erster For loop der über das Array geht
-    for(var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++) {
-        // 2. For Loop der über die sub arrays geht
-     for(var subArrayIndex = 0; subArrayIndex < arr[arrayIndex].length; subArrayIndex++) {
-        // IF Abfrage, welche Nummer größer ist
-        if(arr[arrayIndex][subArrayIndex] > largestNumber[arrayIndex]) {         
-           largestNumber[arrayIndex] = arr[arrayIndex][subArrayIndex];
-         }
+// Simple Solution for Number of Four
+function largestOfFourBasic(arr) {
+   // Erstellen einer Variablen für die Resultate
+   var results = [];
+   // Initialisierung der ersten for Schleife, also der äußere Loop
+   for (var n = 0; n < arr.length; n++) {
+      // Definition der Variable um die largest Number im Arr an der bestimmen Position zu speichern.
+     var largestNumber = arr[n][0];
+     // nächste For Schleife um mit Sub Arrays zu arbeiten 
+     for (var sb = 1; sb < arr[n].length; sb++) {
+       // Einbau des Vergleich, ob die Nummer gröer ist oder nicht, wenn ja wird die größere Nummer gespeichert.
+       if (arr[n][sb] > largestNumber) {
+         largestNumber = arr[n][sb];
+       }
      }
-  }
-  // Rückgabe der Variable, benötigt halt auch Eingabearrays
-  return largestNumber;
-}
-  
-console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
-// To Do, weil derzeit werden die negativen Werte nicht berücksichtigt. 
-// Eingabe von 
-console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]))
-// ergibt: 25, 48, 21, 0
+ 
+     results[n] = largestNumber;
+   }
+   // Rückgabe der Werte
+   return results;
+ }
+
+ console.log(largestOfFourBasic([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]))
+ console.log(largestOfFourBasic([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
