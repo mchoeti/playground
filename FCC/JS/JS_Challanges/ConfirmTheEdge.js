5c6bc64d040c6e8e24dfd808704fe8265e2bbc31
Hoeserle <lastcrusade@gmx.at>
2019-01-03 13:35:31 +0100

    Confirm the Ending with Substring

--- FCC/JS/JS_Challanges/ConfirmTheEdge.js
+++ FCC/JS/JS_Challanges/ConfirmTheEdge.js
@@ -25,9 +25,41 @@ console.log(confirmEndingShort("Bastia", "n"));
 
 // ###########################################
 
-// Test
-// var str = 'To be, or not to be, that is the question.';
+// Simpler Test Test siehe https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith 
+/* var str = 'To be, or not to be, that is the question.';
+console.log(str.endsWith('question.')); // true
+console.log(str.endsWith('to be'));     // false
+console.log(str.endsWith('to be', 19)); // true
+*/
 
-// console.log(str.endsWith('question.')); // true
-// console.log(str.endsWith('to be'));     // false
-// console.log(str.endsWith('to be', 19)); // true
+
+//#############################################################
+// # Solution without endsWith()
+// Verwenden von Substring 
+
+var str = 'Christian';
+var target = 'c'
+
+// Startet bei 2 und gibt 3 buchstaben aus
+console.log(str.substr(2,3));
+// expected output: "ris"
+
+// Startet bei 2 und gibts den Rest aus
+console.log(str.substr(2));
+// expected output: "ristian"
+
+// gibts den letzten Buchstaben aus von Christian  weil target nur ein Zeichen hat
+console.log(str.substr(-target.length));
+
+// ergibt damit n, möchte ich das auf Gültigkeit prüfen, muss eine Abfrage eingebaut werdeb
+console.log(str.substr(-target.length) === target);
+// expected output: false
+
+
+//#################  LÖSUNG ####################
+function confirmEndingShort1(str, target) {
+   // Ich verwende hier substring ==> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substr
+   // Die substr() Methode gibt einen Teil des Strings zuzrück, beginnend am spezifizierten Index 
+    return str.substr(-target.length) === target;
+  }
+  console.log(confirmEndingShort1("Christian", "n"));
