5ead987ddda2df9235bd3178c29438f68adabccd
Mchoeti <lastcrusade@gmx.at>
2019-01-29 01:51:05 +0100

    clean

--- FCC/JS/ConnectWithAPI/scripts.js
+++ FCC/JS/ConnectWithAPI/scripts.js
@@ -17,15 +17,12 @@ app.appendChild(container);
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
-
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 request.onload = function () {
-
     // Begin accessing JSON data here
     var data = JSON.parse(this.response);
-
     if (request.status >= 200 && request.status < 400) {
         data.forEach(movie => {
             console.log(movie.title);
@@ -43,24 +40,19 @@ request.onload = function () {
         // Create a div with a card class
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
-
         // Create an h1 and set the text content to the film's title
         const h1 = document.createElement('h1');
         h1.textContent = movie.title;
-
         // Create a p and set the text content to the film's description
         const p = document.createElement('p');
         movie.description = movie.description.substring(0, 300); // Limit to 300 chars
         p.textContent = `${movie.description}...`; // End with an ellipses
-
         // // Log each movie's title
         // console.log(movie.title);
         // // Log each movie description
         // console.log(movie.description);
-
         // Append the cards to the container element
         container.appendChild(card);
-
         // Each card will contain an h1 and a p
         card.appendChild(h1);
         card.appendChild(p);
