c783a376caa92195598185ce7cf88406615bfbcb
mchoeti <mchoeti@gmail.com>
2020-11-14 08:43:54 +0100

    Preisrechner mit Schleife

--- SimpleJSCod/taskSchleife.js
+++ SimpleJSCod/taskSchleife.js
@@ -34,3 +34,56 @@ for (const index in students) {
 // Ausgeben um zu prüfen des
 console.log("Gruppe1 ", students1);
 console.log("Gruppe2 ", students2);
+
+console.log("*****************************************");
+// Task Nummer 3
+
+// a) Wie viel kostet es für einen Teilnehmer, die Sprache bis 
+//    einschließlich des Levels "C1" zu lernen? Schreibe deinen Code so
+//    universell, dass er auch mit anderen Level-Bezeichnungen / Preisen
+//    zurecht käme.
+//
+// b) Wie viel kostet es, einen Teilnehmer von (einschließlich) A2 bis
+//    einschließlich C1 zu bringen?
+// 
+// c) Ein Teilnehmer möchte (maximal) 1500€ in seine Sprachkenntnisse 
+//    investieren. Bis zu welchem Level (bei A1 angefangen) können wir
+//    ihn dafür unterrichten?
+
+let levels = ["A1", "A2", "B1", "B2", "C1", "C2"];
+let prices = [400, 500, 550, 600, 650, 700];
+
+let totalC1 = 0;
+// Preisrechner wieviel von a bis c
+for (const index1 in levels) {
+    const level = levels[index1];
+    const price = prices[index1];
+
+    //totalC1 = totalC1 + price;
+    // kompaktere Schreibweise
+    totalC1 += price;
+    if (level === "C1") {
+        break;
+    }
+}
+console.log("Preis inklusive C1: ", totalC1);
+
+// Von A2 bis C1:let totalC1 = 0;
+// Preisrechner wieviel von a bis c
+let totalA2C1 = 0;
+let levelA2seen = false;
+for (const index1 in levels) {
+    const level = levels[index1];
+    const price = prices[index1];
+
+    if (level == "A2") {
+        levelA2seen = true;
+    }
+    if (levelA2seen) {
+        totalA2C1 += price;
+    }
+    if (level === "C1") {
+        break;
+    }
+}
+console.log("Pris von A2 bis C1: ", totalA2C1);
