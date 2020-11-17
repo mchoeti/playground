09e805136e5848786df3e3ffacb516ee909d433d
mchoeti <mchoeti@gmail.com>
2020-11-17 16:11:26 +0100

    Erweiterung

--- SimpleJSCod/Funktionen.js
+++ SimpleJSCod/Funktionen.js
@@ -3,6 +3,7 @@
 // heute gehts um Funktionen
 
 // Simple aber Fehlerhaft
+// FUnktionsdefinition
 function sayHello() {
     console.log("Hello World");
     //console.log(name);
@@ -16,4 +17,19 @@ function sayHello2(sayHelloName) {
 }
 sayHello2("Christian");
 sayHello2("Max");
-sayHello2("Erika );
+sayHello2("Erika");
+
+// Funktionsaufruf einer bestehenden Funktion
+console.log(Math.max(1, 2, 3));
+
+// Erweiterung mehrerer Funktionsparameter
+function sayHello2(sayHelloName, language) {
+    if (language === "de") {
+        console.log("Hallo und Willkommen " + sayHelloName + "!");
+    } else if (language === "en") {
+        console.log("Hello and welcome " + sayHelloName + "!");
+    }
+}
+sayHello2("Christian", "de");
+sayHello2("Max", "de");
+sayHello2("Erika", "en");
