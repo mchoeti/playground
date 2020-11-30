af4c7b276000a66aaf9cfd6adc41953d9ddaaf74
mchoeti <mchoeti@gmail.com>
2020-11-30 16:38:25 +0100

    aufräumen

--- SimpleJSCod/Objekte/1_Objekte.js
+++ SimpleJSCod/Objekte/1_Objekte.js
@@ -1,4 +1,13 @@
 //"use strict";
 /*jshint esversion: 6 */
 
-const student = "Max Mustermann";
+// Geschweifte Klammern erstellen ein Objekt
+// Ich schreib meistens immer links die Eigenschaft ohne Klammer
+const student = {
+    name: "Max",
+    alter: 30,
+    kurs: "Deutsch"
+};
+
+console.log(student);
+console.log(student.alter);
