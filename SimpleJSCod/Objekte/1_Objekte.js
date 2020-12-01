1ae332dd350331043de2a18683de63fc2a750980
mchoeti <mchoeti@gmail.com>
2020-12-01 15:49:06 +0100

    Objekte 123

--- SimpleJSCod/Objekte/1_Objekte.js
+++ SimpleJSCod/Objekte/1_Objekte.js
@@ -10,4 +10,32 @@ const student = {
 };
 
 console.log(student);
+
+// Alter hat sich erhöht
+student.alter = 31;
+// neue Eigenschaft setzet
+student.subject = "Englisch";
+
+//console.log(student.alter, student.name);
 console.log(student.alter);
+console.log(student);
+
+// Mal das Alter Löschen
+delete student.alter;
+console.log(student);
+
+
+// bei komplexeren Werten gehen auch Zahlen und Strings
+
+const student2 = {
+    name: "Sissy",
+    alter: 20,
+    kurs: "Deutsch",
+    "2019-2020": "Ungarisch",
+    "gewünschter Kurs": "Portugisisch"
+}
+console.log(student2);
+
+
+const property = "name";
+console.log(student[property]);
