98c2a195699ffa432b9af9ee71cded77bfad4a94
mchoeti <mchoeti@gmail.com>
2020-12-03 19:53:47 +0100

    Objekt

---
+++ SimpleJSCod/Objekte/4_ObjektVerschachtelung.js
@@ -0,0 +1,21 @@
+//"use strict";
+/*jshint esversion: 6 */
+
+// Objekte in Arrays
+const student = [
+    { firstname: "Christian", lastname: "Mustermann" },
+    { firstname: "Doris ", lastname: "Mustermann" },
+];
+
+console.log("student", student);
+
+
+// Objekt klappt nur mit Objekten also mit { } definiert
+// const studentObjekt = {
+//     name: "Max",
+//     numberOfCourses: 0
+// };
+
+// console.log(Object.keys(studentObjekt));
+// console.log(Object.values(studentObjekt));
+// console.log(Object.entries(studentObjekt));
