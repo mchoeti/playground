fa4b172e7709bfbfd420251c33ed62dfca387026
mchoeti <mchoeti@gmail.com>
2020-12-04 16:26:36 +0100

    Objekte und mehr

--- SimpleJSCod/Objekte/4_ObjektVerschachtelung.js
+++ SimpleJSCod/Objekte/4_ObjektVerschachtelung.js
@@ -2,12 +2,34 @@
 /*jshint esversion: 6 */
 
 // Objekte in Arrays
-const student = [
+const students = [
     { firstname: "Christian", lastname: "Mustermann" },
     { firstname: "Doris ", lastname: "Mustermann" },
 ];
 
-console.log("student", student);
+//console.log("student", students);
+
+// Damit kann ich das Objekt auch ausgeben
+for (const student of students) {
+    console.log("Meine Eigenschafen als Student sind: ", student);
+}
+
+// Wer das von früher mit einem i kennt, der kann auch das verwenden.
+console.log("+++++++++++++++++++++++");
+for (const i of students) {
+    console.log("Meine Eigenschafen als Student sind: ", i);
+}
+
+const testObjekt = {
+    items: ["Apfel", "Banane", "Zitrone"]
+}
+console.log("testObjekt", testObjekt);
+
+
+
+
+
+
 
 
 // Objekt klappt nur mit Objekten also mit { } definiert
