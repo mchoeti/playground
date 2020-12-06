87649b02053a15cdd6d30bb9a234d18afb788d99
Mchoeti <lastcrusade@gmx.at>
2020-12-06 21:50:43 +0100

    Simple JSON

--- SimpleJSCod/JSON/1_json.js
+++ SimpleJSCod/JSON/1_json.js
@@ -2,3 +2,17 @@
 /*jshint esversion: 6 */
 
 // JSON ermöglicht es dir ein Objekt als Text auszudrücken
+// von JS to JSON 
+// Array, Objekete, Strings , Zahlen und Bools 
+// In JSON muss ich alle "SChlüssel" als String definieren.
+const data = [{
+        "firstname": "Max",
+        "age": 21,
+        "enrolled": true
+    },
+    {
+        "firstname": "Susi",
+        "age": 35,
+        "enrolled": false
+    },
+];
