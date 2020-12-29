b0ea7e3a89b0792368bf06b62ef4d94222f56905
mchoeti <mchoeti@gmail.com>
2020-12-29 15:25:46 +0100

    Ansätze später aufräumen 114

---
+++ SimpleJSCod/JS_Brower/Projekte/Projekt_t01/mainNew.js
@@ -0,0 +1,24 @@
+//"use strict";
+/*jshint esversion: 6 */
+
+// Das is mal meine Basis
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Hallo Du");
+
+    // Definieren meiner beiden Variablen 
+    const buttonClick = document.getElementById("buttonClick");
+    const buttonReset = document.getElementById("buttonReset");
+
+    const counterNumber = document.getElementById("counter-number");
+    buttonClick.addEventListener("click", () => {
+        const currentCounter = parseInt(counterNumber.innerText, 10);
+        counterNumber.innerText = currentCounter + 1;
+        console.log(currentCounter);
+    });
+
+    buttonReset.addEventListener("click", () => {
+        console.log("Hallo");
+        counterNumber.innerText = "0";
+    });
+
+});
