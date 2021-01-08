71f83ab982627be30c3fb9c43d8c393f818b3e96
mchoeti <mchoeti@gmail.com>
2021-01-08 12:39:05 +0100

    Extra Attribute

--- SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/main2.js
+++ SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/main2.js
@@ -11,4 +11,21 @@ document.addEventListener("DOMContentLoaded", () => {
     // UNd hier holen wir uns mal alle eine komplexen CSS Selector
     const cardHeadings = document.querySelectorAll(".card div.card-header");
     console.log(cardHeadings);
+
+    // Alle abholen und dann das erste anzeigen
+    const firstCardHeader = document.getElementsByClassName("card-header")[0];
+    //Attributes gibts auf allen HTML Elementen ==> Listed Namend nodes MAP
+    // console.log(firstCardHeader.attributes[0]);
+    // console.log(firstCardHeader.attributes[1]);
+    // Oder Alle
+    // console.log("Oder alle", firstCardHeader.attributes);
+    // console.log(firstCardHeader.attributes["class"]);
+    // oder alle
+    console.log(firstCardHeader.attributes.class);
+    // unser Data Extra
+    console.log(firstCardHeader.attributes["data-extra"]);
+
+    // Und damit kann ich mir auch gleich mal "nur" den Value raussuchen.
+    const dataExtraAttribute = firstCardHeader.attributes["data-extra"].value;
+    console.dir(dataExtraAttribute);
 });
