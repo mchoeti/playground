d73c0e733eabe3957c778805075cb268be8aebe6
mchoeti <mchoeti@gmail.com>
2021-01-08 18:44:50 +0100

    Hepl added

--- SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/main4.js
+++ SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/main4.js
@@ -1,7 +1,25 @@
 //"use strict";
 /*jshint esversion: 6 */
 
-// Query Selector klappen mit CSS 
+// Query Selector klappen mit CSS
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Willkommen bei einem neuen JS Projekt");
+
+    // hier hol ich mir noch einen CSS Selector raus. Siehe auch style4.css
+    const helpElements = document.querySelectorAll("[data-help]");
+    console.log(helpElements);
+
+    for (const helpElement of helpElements) {
+        // Sicherstellen das Element existiert bzw. nicht existierenden keine ERRORs werfen.
+        if (!helpElement.attributes["data-help"]) {
+            continue;
+        }
+
+        helpElement.addEventListener("click", (event) => {
+            //alert("Help wurde geklickt");
+            const helpText = helpElement.attributes["data-help"].value;
+            console.log(helpText);
+            alert(helpText);
+        });
+    }
 });
