0c172c571d17084e6313333c41106728845a858b
mchoeti <mchoeti@gmail.com>
2021-01-08 12:47:37 +0100

    Neues Projekt HIlfetexte entwickeln

---
+++ SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/main4.js
@@ -0,0 +1,7 @@
+//"use strict";
+/*jshint esversion: 6 */
+
+// Query Selector klappen mit CSS 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Willkommen bei einem neuen JS Projekt");
+});
