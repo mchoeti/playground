562384e066ff98f0b8c8789a3deae5c52f8471c1
mchoeti <mchoeti@gmail.com>
2021-01-14 17:26:44 +0100

    Style angepasst. NAch Klick display none

---
+++ SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/main7.js
@@ -0,0 +1,36 @@
+//"use strict";
+/*jshint esversion: 6 */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const helpElements = document.querySelectorAll("[data-help]");
+    for (const helpElement of helpElements) {
+        helpElement.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const helpText = helpElement.attributes["data-help"].value;
+            alert(helpText);
+        });
+    }
+
+    const cardElements = document.getElementsByClassName("card");
+    for (const cardElement of cardElements) {
+
+        // if (cardElement.style["background-color"] === "") {
+        //     cardElement.style["background-color"] = "darkblue"
+        // }
+        console.log(cardElement.style);
+        console.log("Farbe des Style Elements background: ", cardElement.style.backgroundColor);
+
+        if (cardElement.style.backgroundColor === "") {
+            cardElement.style.backgroundColor = "darkred";
+        }
+
+        // Display soll verschwinden. Damit kann ich ein ELement ausblenden.
+        cardElement.addEventListener("click", () => {
+            cardElement.style.display = "none";
+        });
+
+    }
+
+});
