1fbf41d80a1e26835fd9ee462a2a0e98e4fa3adf
mchoeti <mchoeti@gmail.com>
2021-01-18 17:09:02 +0100

    Ein und ausklappen Togglen und mehr

--- SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/taskMain.js
+++ SimpleJSCod/JS_Brower/Projekte/Projekt_JSiB02/js/taskMain.js
@@ -12,5 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    const cardHeaders = document.getElementsByClassName("card-header");
+    console.log("cardHeaders");
+    for (const cardHeader of cardHeaders) {
+        cardHeader.addEventListener("click", (event) => {
+            console.log("Du hast geklickt");
+            const cardBody = cardHeader.nextElementSibling;
+            console.log("Du hast geklickt und das is der Body: ", cardBody);
 
+            // EInfaches uns simples Ausblenden
+            //cardBody.style.display = "none";
+
+            // Via Bootstrap damit kann ich auch noch eine Klasse hinzufügen.
+            //cardBody.classList.add("d-none");
+
+            // Togglen also hinzufügen oder entfernen
+            cardBody.classList.toggle("d-none");
+
+        });
+    }
 });
