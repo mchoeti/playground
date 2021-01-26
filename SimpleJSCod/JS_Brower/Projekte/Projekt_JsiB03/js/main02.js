39be6938229d27486f809ce54c02398da3ca905f
mchoeti <mchoeti@gmail.com>
2021-01-26 17:30:22 +0100

    added some remove functions now first in first out

--- SimpleJSCod/JS_Brower/Projekte/Projekt_JsiB03/js/main02.js
+++ SimpleJSCod/JS_Brower/Projekte/Projekt_JsiB03/js/main02.js
@@ -15,12 +15,23 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(lastElement);
     });
 
+
     // Trying to remove the button
     const removeStudentButton = document.getElementById("remove-student");
     // Funktion eingebaut
     removeStudentButton.addEventListener("click", () => {
         console.log("Und nun hast du den Remove Button geklickt");
+        //console.log(lastElement);
+        const removeLiElement = document.querySelectorAll("li");
+        console.log(removeLiElement);
+        // Damit entferne ich immer das 1. Element :-)
+        // Also First in first out :-)
+        removeLiElement[0].remove();
 
+        // und dann noch ein2 h2
+        const removeH2 = document.querySelectorAll("h2");
+        console.log(removeH2);
+        removeH2[0].remove();
     });
 
 
