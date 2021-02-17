addd84cee67c607f12f11be0d5fbf037370d6fa5
mchoeti <mchoeti@gmail.com>
2021-02-17 15:48:26 +0100

    Obejcts in JS Vorbereiten

---
+++ SimpleJSCod/JS_Brower/Projekte/JS_Objects/objects01.js
@@ -0,0 +1,7 @@
+//"use strict";
+/*jshint esversion: 6 */
+// Mit STRG und Leertaste gibts die Autocomplete  // kleine Update
+// a normales F5 reicht auch 
+
+
+console.log("Hello World im am back");
