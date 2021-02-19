5bc568204f9d712f1d48aba6b4330606ea1aebe1
mchoeti <mchoeti@gmail.com>
2021-02-19 21:10:40 +0100

    Todos Part1

--- SimpleJSCod/JS_Brower/Projekte/JS_Objects/objects01.js
+++ SimpleJSCod/JS_Brower/Projekte/JS_Objects/objects01.js
@@ -3,5 +3,27 @@
 // Mit STRG und Leertaste gibts die Autocomplete  // kleine Update
 // a normales F5 reicht auch 
 
-
 console.log("Hello World im am back");
+
+const data = {
+    todos: ["JS Kurs anpassen", "Node JS anpassen"],
+    addTo: function(Argument) {
+        // und jetzt verwenden wir this anstelle von data
+        // this ist also der Kontext der Funktion
+        //data.todos.push(Argument);
+        this.todos.push(Argument);
+    },
+    printTodos: function() {
+        console.log(this.todos);
+    }
+};
+
+data.addTo("3. TO Do");
+data.addTo("4. TO Do");
+
+for (var i = 5; i <= 7; i++) {
+    data.addTo([i] + ".es TO Do");
+}
+
+data.printTodos();
+//console.log(data);
