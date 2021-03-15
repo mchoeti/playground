8e9683c4fcdb4ccb3c223e2ad84c0cc7deaee10a
mchoeti <mchoeti@gmail.com>
2021-03-15 16:35:51 +0100

    implemented some things

---
+++ SimpleJSCod/Schleifen/01_simpleLoops.js
@@ -0,0 +1,21 @@
+for (var i = 0; i <= 10; i++) {
+    console.log("Gimme For Loop", i);
+}
+
+console.log("###############################");
+
+var counter = 0;
+while (counter < 11) {
+    console.log("While", counter);
+    counter++;
+}
+
+
+var stars = "";
+var starCount = 0;
+
+while (starCount < 9) {
+    stars = stars + "*";
+    console.log(stars);
+    starCount++;
+}
