3fb16e873f55c3087b411cabf1862f73ffb5a5ea
mchoeti <mchoeti@gmail.com>
2021-03-29 17:41:10 +0200

    Added Basic HTML

--- SimpleJSCod/JS_Brower/API_New/website/sketch.js
+++ SimpleJSCod/JS_Brower/API_New/website/sketch.js
@@ -10,15 +10,28 @@ function gotData(data) {
     console.log(data);
     var keys = Object.keys(data);
     console.log(keys);
-    for (var i = 0; i < keys.length; i++) {
-        console.log("KLassicher FOR Loop : ", i);
-        var word = keys[i];
+    // for (var i = 0; i < keys.length; i++) {
+    //     console.log("KLassicher FOR Loop : ", i);
+    //     var word = keys[i];
+    //     var score = data[word];
+    //     var x = random(width);
+    //     var y = random(height);
+    //     fill(255);
+    //     textSize(22);
+    //     text(word, x, y);
+    // }
+
+    // Alternative mit einem For Each Loop
+    keys.forEach(element => {
+        console.log("Aber sowas von FOR EACH", element);
+        var word = element;
         var score = data[word];
         var x = random(width);
         var y = random(height);
-        fill(255);
+        fill(0, 200, 153);
+        textSize(22);
         text(word, x, y);
-    }
+    });
 
 }
 
