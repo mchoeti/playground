9ce6025b365471c9ca827fb492368b7fa246ac82
Mchoeti <lastcrusade@gmx.at>
2021-07-29 18:15:26 +0200

    updates , some and every

--- SimpleJSCod/JS_Brower/API/02_index_test.js
+++ SimpleJSCod/JS_Brower/API/02_index_test.js
@@ -1,7 +1,13 @@
 //"use strict";
 /*jshint esversion: 6 */
 
+const myNumbers = [1102747, 1102748];
+
 const axios = require("axios").default;
+myNumbers.forEach(element => {
+    console.log("Call 1 ", element);
+});
+
 axios.get("https://api.nal.usda.gov/fdc/v1/food/1102747", {
         params: {
             api_key: "PtTpskZ8Xa5Mixq7qAhs9LizA9v9jSMHRzwebVTI"
--- SimpleJSCod/JS_Brower/API/readme.md
+++ SimpleJSCod/JS_Brower/API/readme.md
@@ -3,7 +3,7 @@ Simple Sample für eine API.
 Endpunkt:  
 https://api.nal.usda.gov/fdc/v1/foods/list?api_key=xxxxxxxxxxxxxxxxxxxxx
 
-- zuerst  npm init
-- dann file packagge.json schließen 
-- Axios installieren 
-``npm install --save axios ``
+- zuerst npm init
+- dann file packagge.json schließen
+- Axios installieren
+  `npm install --save axios `
